6e50ea441bcc66fa99bb774f6556ddc04b45a740
Sunday <139761689+Light1015@users.noreply.github.com>
2025-09-22 14:22:52 +0700

    blog-api

---
+++ blog-api/routes/posts.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const { createPost, getPostWithComments } = require('../controllers/postController');
+router.post('/', createPost);
+router.get('/:id', getPostWithComments);
+module.exports = router;
